490eb9ff38ab07484a274a073127b737aaac1027
Vilim Branica <vbranica@gmail.com>
2021-11-29 18:29:13 +0100

    Započeo users app

--- JobSearch/JobSearch/urls.py
+++ JobSearch/JobSearch/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jobs/', include('jobs.urls'))
+    path('jobs/', include('jobs.urls')),
+    path('users/', include('users.urls')),
 ]
