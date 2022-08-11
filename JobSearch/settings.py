e14a2fc405cd3796f1372ae078ddf114a68ca053
Vilim Branica <vbranica@gmail.com>
2022-08-11 22:32:42 +0200

    "Changed settings.py and added new files for heroku deployment"

--- JobSearch/settings.py
+++ JobSearch/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = os.getenv('SECRET_KEY', 'abcd')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn JobSearch.wsgi --log-file -
