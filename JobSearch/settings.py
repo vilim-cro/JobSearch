a19910336b1234fc9ac605678d09e8fbc4780df5
Vilim Branica <vbranica@gmail.com>
2022-08-11 22:42:05 +0200

    Added allowed hosts to settings.py

--- JobSearch/settings.py
+++ JobSearch/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'abcd')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'vilimcro-jobsearch.herokuapp.com',
+    '127.0.0.1'
+    ]
 
 
 # Application definition
